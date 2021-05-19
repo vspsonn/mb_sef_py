f3fdb786e0bbe37991e2163d6b6119e723a1d02b
vspsonn <84449280+vspsonn@users.noreply.github.com>
2021-05-19 13:16:57 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+# DEFINE VERSION NUMBER
+major = '0'
+minor = '0'
+patch = '0'
+
+setup(name='mbs_sef_py',
+      version= major + '.' + minor + '.' + patch,
+      license="",
+      description='mbs_sef_py',
+      packages=find_packages(),
+      install_requires=[]
+      )
